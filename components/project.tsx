973f1032fae197f1aae9fb724e315b15d4ef4c28
Sai Subrahmanyam Mahadasa <143134515+saiMahadasa@users.noreply.github.com>
2024-10-29 12:36:39 -0500

    Project UI in container

--- components/project.tsx
+++ components/project.tsx
@@ -10,32 +10,31 @@ export default function Project({
   title,
   description,
   tags,
-  imageUrl,
 }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["0 1", "1.33 1"],
   });
-  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
   return (
     <motion.div
       ref={ref}
       style={{
-        scale: scaleProgess,
-        opacity: opacityProgess,
+        scale: scaleProgress,
+        opacity: opacityProgress,
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-full border border-black/5 rounded-lg overflow-hidden p-5 sm:p-8 relative h-auto hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="flex flex-col h-full">
+      <section className="bg-gray-100 max-w-[42rem] mx-auto border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 flex flex-col h-full">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-1 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
-          <ul className="flex flex-wrap mt-4 gap-2">
+          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
               <li
                 className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
@@ -46,17 +45,7 @@ export default function Project({
             ))}
           </ul>
         </div>
-
-        {/* If you want to add the image back uniformly, use the following */}
-        {/* <Image
-          src={imageUrl}
-          alt="Project I worked on"
-          quality={95}
-          className="mt-8 rounded-t-lg shadow-2xl w-full sm:w-[28.25rem] transition-transform
-            hover:scale-[1.04] hover:-translate-x-3 hover:translate-y-3 hover:rotate-2"
-        /> */}
       </section>
     </motion.div>
   );
 }
-
