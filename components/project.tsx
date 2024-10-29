d86e5015da205464b870947ed4f793e5a0aa9ade
Sai Subrahmanyam Mahadasa <143134515+saiMahadasa@users.noreply.github.com>
2024-10-29 12:45:59 -0500

    project ui update

--- components/project.tsx
+++ components/project.tsx
@@ -31,7 +31,7 @@ export default function Project({
       <section className="bg-gray-100 max-w-[42rem] mx-auto border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 flex flex-col h-full">
           <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-1 leading-relaxed text-gray-700 dark:text-white/70">
+          <p className="mt-1 mb-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
