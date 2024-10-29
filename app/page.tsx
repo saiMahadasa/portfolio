88c4a5fe214cc64f4bae4e8e8baeeeadf0f8283f
Sai Subrahmanyam Mahadasa <143134515+saiMahadasa@users.noreply.github.com>
2024-10-29 14:31:44 -0500

    Chatbot removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import About from "@/components/about";
-import Chatbot from "@/components/chatbot";
 import Contact from "@/components/contact";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
@@ -17,7 +16,7 @@ export default function Home() {
       <Skills />
       <Experience />
       <Contact />
-      <Chatbot/>
+      
     </main>
   );
 }
