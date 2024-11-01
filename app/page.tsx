85c850db48e85cc84b2f71720e662b11955a402b
Sai Subrahmanyam Mahadasa <143134515+saiMahadasa@users.noreply.github.com>
2024-11-01 02:23:15 -0500

    Update chatbot added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/about";
 import Contact from "@/components/contact";
 import Experience from "@/components/experience";
+import Chatbot from "@/components/Chatbot";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
@@ -16,7 +17,7 @@ export default function Home() {
       <Skills />
       <Experience />
       <Contact />
-      
+      <Chatbot/>
     </main>
   );
 }
